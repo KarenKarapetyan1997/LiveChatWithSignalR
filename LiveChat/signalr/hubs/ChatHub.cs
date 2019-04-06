47245fdabb325f99d2ff47f66a4e08eb487810fb
karen.karapetyan1997 <karen.karapetyan1997@gmail.com>
2019-04-07 02:05:00 +0400

    Code

---
+++ LiveChat/signalr/hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Owin;
+using Microsoft.Owin;
+
+namespace LiveChat.signalr.hubs
+{
+    public class ChatHub : Hub
+    {
+       public void Send(string name, string message)
+        {
+            Clients.All.addNewMessageToPage(name,message);
+        }
+     
+    }
+}
